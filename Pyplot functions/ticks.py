11ce138486bf67f73aee1a7b07c830dc24e709fe
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-29 00:37:55 +0600

    it helps us to read the data more carefully and more specific

---
+++ Pyplot functions/ticks.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+months = [1, 2, 3, 4]
+sales = [1000, 1500, 1200, 1800]
+
+plt.plot(months, sales, color = 'blue', linestyle = '--', linewidth = 2, marker = 'o', label = '2025 sales data')
+plt.xlabel('Months')
+plt.ylabel('Sales per month')
+plt.title('Monthly sales Data report')
+plt.legend(loc = 'upper left',fontsize = 12)
+plt.grid(color = 'gray',linestyle =':',linewidth = 1)
+plt.xlim(1,4)
+plt.ylim(0,2000)
+plt.xticks([1, 2, 3, 4], ['M1', 'M2', 'M3', 'M4'])
+plt.show()
