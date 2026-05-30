5d02c4c294efb542a7e89d4f5bc39a29098ceb64
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-30 21:41:21 +0600

    learnt two things about subplot

---
+++ Subplot and Layout Adjustments/making_subplot.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4]
+y = [10,20,15,25]
+
+plt.subplot(1,2,1) # 1row, 2column, 1st subplot
+plt.plot(x,y)
+plt.title('Line Chart')
+
+plt.subplot(1,2,2) # 1row, 2column, 2nd subplot
+plt.bar(x,y)
+plt.title('Bar Chart')
+
+plt.tight_layout
+plt.show()
