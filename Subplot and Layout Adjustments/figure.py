654317c243fda7317b9a48d403b72987ea3a6589
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-30 21:45:25 +0600

    and now here we also get known how to add the subtitles of these two charts

--- Subplot and Layout Adjustments/figure.py
+++ Subplot and Layout Adjustments/figure.py
@@ -5,11 +5,13 @@ fig, ax = plt.subplots(1,2, figsize=(10,5))
 x = [1,2,3,4]
 y = [10,20,15,25]
 
-ax[0].plot(x,y)
+ax[0].plot(x,y, color='blue')
 ax[0].set_title('Line Plot')
 
-ax[1].bar(x,y)
+ax[1].bar(x,y, color='green')
 ax[1].set_title('Bar Chart')
+
+fig.suptitle('Comparison of Line and Bar Chart')
 
 plt.tight_layout()
 plt.show()
