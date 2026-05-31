6ecf724315de835f7897adc34cafe0f69ca14d07
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-31 22:48:02 +0600

    ahhhh things are getting crazy and get's a lot of fun

---
+++ Practice with a huge database/project_of_matplotlib.py
@@ -0,0 +1,29 @@
+# s-1 import the ilbrairies
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# load the data
+df = pd.read_csv(r"D:\Matplotlib\Practice with a huge database\netflix_data.csv")
+
+# clean data
+df = df.dropna(subset=['type','title','director','cast','director','duration','listed_in','description','release_year','rating','show_id'])
+
+type_counts = df['type'].value_counts()
+plt.figure(figsize=(6,4))
+plt.bar(type_counts.index, type_counts.values, color=['skyblue','orange'])
+plt.title('Number of Movies VS TV Shows on Netflix')
+plt.xlabel('Type')
+plt.ylabel('Count')
+plt.tight_layout()
+plt.savefig('movies_vs_tvshows.png')
+plt.show()
+
+rating_counts = df['rating'].value_counts()
+plt.figure(figsize=(8,6))
+plt.pie(rating_counts, labels=rating_counts.index, autopct='%1.1f%%', startangle=90)
+plt.title('Percentage of Content Ratings')
+plt.xlabel('Type')
+plt.ylabel('Count')
+plt.tight_layout()
+plt.savefig('conetent_rating.png')
+plt.show()
